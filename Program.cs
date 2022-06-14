1f4f3eb4a1213066c1422a4a6cb2db12baf80857
Reight-DKC <reightultimate@gmail.com>
2022-06-14 17:46:04 -0600

    Area of rectangle complete

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Rectangle Area
+Console.WriteLine("Base = 7 cm");
+
+Console.WriteLine("Altitude = 14 cm");
+
+Console.WriteLine("\nRectangle Area: Base * Altitude");
+
+Console.WriteLine("\nRectangle Area: 7 cm * 14 cm");
+
+Console.WriteLine("\nArea is:");
+Console.WriteLine(7 * 14);
